16c685f7ced7af8ecbd9596dc2d26c8b82440537
RussianFox <horonitel@gmail.com>
2019-10-11 12:36:43 +0000

    Fix setup

--- server/setup.sh
+++ server/setup.sh
@@ -3,7 +3,7 @@ mkdir -p /srv/www/htdocs/roadsituation
 ln -s /git/roadsituation/web /srv/www/htdocs/roadsituation/map
 ln -s /git/roadsituation/server/htdocs /srv/www/htdocs/roadsituation/server
 ln -s /git/roadsituation/server/nginx/conf.d/vhosts.conf  /etc/nginx/conf.d/vhosts.conf
-ln -s /git/roadsituation/server/nginx/vhosts.d  /etc/nginx/conf.d/vhosts.d
+ln -s /git/roadsituation/server/nginx/vhosts.d  /etc/nginx/vhosts.d
 ln -s /git/roadsituation/server/php-fpm/roadsituation.conf /etc/php-fpm.d/roadsituation.conf
 cd /srv/www/htdocs/roadsituation/server/staff
 sh ./composer_install.sh
