7c67ba50bed9a7571037468671b15cb9f8d6bbe5
RussianFox <horonitel@gmail.com>
2019-10-11 13:18:35 +0000

    add secondary elasticsearch node configuration

---
+++ elasticsearch/setup_secondary_node.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+systemctl stop elasticsearch
+rm -rf /var/lib/elasticsearch/*
+rm /etc/elasticsearch/elasticsearch.yml
+cp /git/elasticsearch/elasticsearch_secondary.yml /etc/elasticsearch/elasticsearch.yml
+systemctl start elasticsearch
