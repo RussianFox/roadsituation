f97259e26489b47a941d9c507abe45f70cb55c3e
RussianFox <horonitel@gmail.com>
2020-02-27 11:02:26 +0000

    Fix script icentreby

--- server/htdocs/scripts/set_i.centr.by.php
+++ server/htdocs/scripts/set_i.centr.by.php
@@ -10,8 +10,6 @@ function numberFormat($digit, $width) {
 
 echo "Start ".date('Y-m-d H:i:s')."\r\n";
 
-$pages=['a','s','o'];
-
 $index="icentreby";
 
 $ids = array();
@@ -23,28 +21,48 @@ $file=@file_get_contents($url);
 
 if ($file) {
 	echo "$url loaded \r\n";
+	$file = str_replace('}"', '}', $file);
+	$file = str_replace('"{', '{', $file);
+	$file = str_replace('\"', '"', $file);
 	$file = json_decode($file, true);
 	
 	foreach ($file as $feature) {
 	
-	    $object_id=$feature['idRd'];
-		
+		$object_id=$feature['idRd'];
 		$type="maintenance";
 		$name = "Road repair ".$feature['roadNom']." ".$feature['roadTitle']." (".$feature['kmBegin']."-".$feature['kmEnd']."km) <br>".$feature['dateBegin']." - ".$feature['dateEnd'];
-		$link="https://transport.mos.ru/";
+		$link="http://i.centr.by/inforoads/ru/repairs";
 		$geometry=$feature['geometry'];
 		
-	    $coordinates=array();
-	    foreach ($geometry['coordinates'] as $coordinate) {
-			array_push($coordinates,array('lat'=>$coordinate[0],"lng"=>$coordinate[1]));
-	    };
-	    $center = center_line($coordinates);
+		//echo $object_id." ".$geometry['type']."\r\n";
+		
+		$coordinates=array();
+		If ($geometry['type'] == "Point") {
+			$center = array('lat'=>$geometry['coordinates'][1],"lng"=>$geometry['coordinates'][0]);
+		} elseif ($geometry['type'] == "LineString") {
+			foreach ($geometry['coordinates'] as $coordinate) {
+				array_push($coordinates,array('lat'=>$coordinate[1],"lng"=>$coordinate[0]));
+			};
+			$center = center_line($coordinates);
+		} else if ($geometry['type'] == "MultiLineString") {
+			foreach ($geometry['coordinates'] as $coords) {
+			    foreach ($coords as $coordinate) {
+				array_push($coordinates,array('lat'=>$coordinate[1],"lng"=>$coordinate[0]));
+			    };
+			};
+			$center = center_line($coordinates);
+		} else {
+		    continue;
+		}
 
-	    update_object_int($object_id,$center['lng'],$center['lat'],$type,$name,null,$link,$index,$geometry);
-	    $ids[] = $object_id;
+		update_object_int($object_id,$center['lng'],$center['lat'],$type,$name,null,$link,$index,$geometry);
+		$ids[] = $object_id;
 	}
 	
-	clean_objects($index,'must_not',$ids);
+	echo "Loading objects success \r\n";
+	print_r($ids);
+	//clean_objects($index,'must_not',$ids);
+	//echo "Cleaning success \r\n";
 	replace_index_alias($index,"roadsituation_icentreby");
 } else {
 	echo "Load $url failed \r\n";
