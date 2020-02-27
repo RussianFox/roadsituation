d54528e52925d7cf31453eaf369549f9b34b7cf7
RussianFox <horonitel@gmail.com>
2020-02-27 14:35:58 +0300

    Fixes script for transportmos

--- server/htdocs/scripts/set_transportmos.php
+++ server/htdocs/scripts/set_transportmos.php
@@ -46,49 +46,46 @@ Accept-Language: en-US,en;q=0.9,ru;q=0.8
     $file=false;
     $file=@file_get_contents($url,null,$context);
     if ($file) {
-	echo "loaded \r\n";
-	$file = json_decode($file, true);
-	$html=$file['html'];
-	$list = preg_match_all('/<li id="closure_id_.*<span class="sp1">(.*)<\/span>.*<span class="sp2">(.*)<\/span>.*class="moreInfo_block">(.*)<\/div>.*<\/li>/sUsi',$html,$matches);
-	
-	foreach ($file['json']['features'] as $feature) {
-	
-	    $object_id=$feature['options']['object_id'];
-	    if (strpos($object_id,'_')) {
-		continue;
-	    };
-	    $type="block";
-	    $name=$feature['properties']['hintContent']." Type:".$feature['options']['type']." Color: ".$feature['options']['iconColor'].", ".$feature['options']['strokeColor'][0].", ".$feature['options']['strokeColor'][1];
-	    $name=$feature['properties']['hintContent'];
-	    
-	    $kk = array_search($name,$matches[2]);
-	    if ($kk) {
-		$name = strip_tags(trim($matches[2][$kk])."\r\n ".trim($matches[1][$kk])."\r\n ".trim($matches[3][$kk]));
-		$name = preg_replace('/\s+/', ' ', $name);
-	    }
-	    
-	    $link="https://transport.mos.ru/";
-	    $coordinates=array();
-	    $geometry="";
-	    foreach ($feature['geometry']['coordinates'] as $coordinate) {
-		array_push($coordinates,array('lat'=>$coordinate[0],"lng"=>$coordinate[1]));
-		$geometry = $geometry.($geometry=="" ? "" : ", ").$coordinate[0]." ".$coordinate[1];
-	    };
-	    $geometry=$feature['geometry']['type']." (".$geometry.")";
-	    $center = center_line($coordinates);
-	    if ($feature['options']['strokeColor'][1]=="#000000") {
-		$type="block";
-	    } else {
-		$type="maintenance";
-	    }
-	    update_object_int($object_id,$center['lng'],$center['lat'],$type,$name,null,$link,$index,$geometry);
-	    $ids[] = $object_id;
-	}
-	
-	clean_objects($index,'must_not',$ids);
-	replace_index_alias($index,"roadsituation_transportmos");
+		echo "loaded \r\n";
+		$file = json_decode($file, true);
+		$html=$file['html'];
+		$list = preg_match_all('/<li id="closure_id_.*<span class="sp1">(.*)<\/span>.*<span class="sp2">(.*)<\/span>.*class="moreInfo_block">(.*)<\/div>.*<\/li>/sUsi',$html,$matches);
+		
+		foreach ($file['json']['features'] as $feature) {
+		
+			$object_id=$feature['options']['object_id'];
+			if (strpos($object_id,'_')) {
+				continue;
+			};
+			$type="block";
+			$name=$feature['properties']['hintContent']." Type:".$feature['options']['type']." Color: ".$feature['options']['iconColor'].", ".$feature['options']['strokeColor'][0].", ".$feature['options']['strokeColor'][1];
+			$name=$feature['properties']['hintContent'];
+			
+			$kk = array_search($name,$matches[2]);
+			if ($kk) {
+				$name = strip_tags(trim($matches[2][$kk])."\r\n ".trim($matches[1][$kk])."\r\n ".trim($matches[3][$kk]));
+				$name = preg_replace('/\s+/', ' ', $name);
+			}
+			
+			$link="https://transport.mos.ru/";
+			$coordinates=array();
+			foreach ($feature['geometry']['coordinates'] as $coordinate) {
+				array_push($coordinates,array('lat'=>$coordinate[1],"lng"=>$coordinate[0]));
+			};
+			$center = center_line($coordinates);
+			if ($feature['options']['strokeColor'][1]=="#000000") {
+				$type="block";
+			} else {
+				$type="maintenance";
+			}
+			update_object_int($object_id,$center['lng'],$center['lat'],$type,$name,null,$link,$index,$feature['geometry']);
+			$ids[] = $object_id;
+		}
+		
+		clean_objects($index,'must_not',$ids);
+		replace_index_alias($index,"roadsituation_transportmos");
     } else {
-	echo "failed \r\n";
+		echo "failed \r\n";
     }
 echo "Finish ".date('Y-m-d H:i:s')."\r\n";
 echo "----------------------------------------\r\n";
