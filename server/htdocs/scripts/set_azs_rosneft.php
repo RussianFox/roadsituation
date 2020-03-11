d87a57b8841a308c3c523eb64b4a32c36af5f989
RussianFox <horonitel@gmail.com>
2020-03-11 14:39:09 +0300

    Add cleaning in azs scripts

--- server/htdocs/scripts/set_azs_rosneft.php
+++ server/htdocs/scripts/set_azs_rosneft.php
@@ -11,12 +11,20 @@ function numberFormat($digit, $width) {
 echo "Start ".date('Y-m-d H:i:s')."\r\n";
 
 $index = "azs_rosneft";
-
+$index_alias="roadsituation_$index"; //FALSE if no need
 $params = ['index' => $index];
 $bool=$client->indices()->exists($params);
 if (!$bool) {
     $response = $client->indices()->create($params);
 }
+$bool=$client->indices()->exists(['index' => $index]);
+if (!$bool) {
+	echo "Index is not exist, creating it \r\n";
+    $response = $client->indices()->create(['index' => $index]);
+}
+$query = $client->count(['index' => $index]);
+$docsCount_start=1*$query['count'];
+$ids=false;
 
 $context = stream_context_create(array(
     'http' => array(
@@ -34,15 +42,10 @@ Accept-Encoding: gzip, deflate, br
 
 $url = "http://www.rosneft-azs.ru/map_search";
 
-
-
-    $ids=false;
-    $file=false;
-    $file=@file_get_contents($url,null,$context);
-    if ($file) {
+$file=false;
+$file=@file_get_contents($url,null,$context);
+if ($file) {
 	$file = json_decode ( $file, TRUE, 15 );
-//	var_dump($file['stations']);
-//	return;
 	foreach ($file['stations'] as $station) {
 	    $id = $station['id'];
 	    $lat=1*trim($station['lat']);
@@ -52,13 +55,27 @@ $url = "http://www.rosneft-azs.ru/map_search";
     	    $link = "http://www.rosneft-azs.ru";
 	    update_object_int($id,$lng,$lat,$type,$name,null,$link,$index);
 	    $ids[]=$id;
-	}
-    } else {
+	};
+	echo "Loading objects success \r\n";
+	
+	$query = $client->count(['index' => $index]);
+	$docsCount_add=1*$query['count'];
+	$docsCount_clean=$docsCount_add;
+	if (((count($ids))/$docsCount_start)*100 > 70) {
+		clean_objects($index,'must_not',$ids);
+		echo "Cleaning success \r\n";
+		$query = $client->count(['index' => $index]);
+		$docsCount_clean=1*$query['count'];
+	} else {
+		echo "Cleaning cancelled \r\n";
+	};
+	
+	if ($index_alias) { replace_index_alias($index,$index_alias); };
+	echo "Statistics. Docs added: ".($docsCount_add-$docsCount_start)." Docs cleaned: ".($docsCount_add-$docsCount_clean)." Docs now: ".$docsCount_clean."\r\n";
+} else {
 	echo "Failed to load ".$url." \r\n";
-    }
+};
 
-clean_objects($index,'must_not',$ids);
-replace_index_alias($index,"roadsituation_$index");
 echo "Finish ".date('Y-m-d H:i:s')."\r\n";
 echo "----------------------------------------\r\n";
 ?>
--- server/htdocs/scripts/set_azs_shell.php
+++ server/htdocs/scripts/set_azs_shell.php
@@ -11,12 +11,20 @@ function numberFormat($digit, $width) {
 echo "Start ".date('Y-m-d H:i:s')."\r\n";
 
 $index = "azs_shell";
-
+$index_alias="roadsituation_$index"; //FALSE if no need
 $params = ['index' => $index];
 $bool=$client->indices()->exists($params);
 if (!$bool) {
     $response = $client->indices()->create($params);
 }
+$bool=$client->indices()->exists(['index' => $index]);
+if (!$bool) {
+	echo "Index is not exist, creating it \r\n";
+    $response = $client->indices()->create(['index' => $index]);
+}
+$query = $client->count(['index' => $index]);
+$docsCount_start=1*$query['count'];
+$ids=false;
 
 $context = stream_context_create(array(
     'http' => array(
@@ -32,33 +40,46 @@ $url = "https://locator.shell.com.ru/deliver_country_csv.csv?footprint=RU&site=c
 $file=false;
 $file=@file($url,FALSE,$context);
 
-    $ids = array();
-    if ($file) {
+if ($file) {
 	echo "File loaded \r\n";
 	foreach ($file as $string) {
-	    $station = explode('	',$string);
-	    if (count($station)< 44) {
+		$station = explode('	',$string);
+		if (count($station)< 44) {
 		continue;
-	    };
-	    //$id = floatval(preg_replace('/[^0-9\.]/', '', $station[43]));
-	    //if ($id == 0) {
+		};
+		//$id = floatval(preg_replace('/[^0-9\.]/', '', $station[43]));
+		//if ($id == 0) {
 		//continue;
-	    //}
-	    $lat=floatval(preg_replace('/[^0-9\.]/', '', $station[5]));
-    	    $lng=floatval(preg_replace('/[^0-9\.]/', '', $station[6]));
-    	    $type='azs';
-    	    $name = "Shell ".$station[0]." ".$station[4]." ".$station[2]." ".$station[1];
-    	    $link = "https://www.shell.com.ru/";
-    	    $id=$station[0];
-	    update_object_int(''.$id,$lng,$lat,$type,$name,null,$link,$index);
-	    $ids[]=$id;
-	}
-    } else {
+		//}
+		$lat=floatval(preg_replace('/[^0-9\.]/', '', $station[5]));
+			$lng=floatval(preg_replace('/[^0-9\.]/', '', $station[6]));
+			$type='azs';
+			$name = "Shell ".$station[0]." ".$station[4]." ".$station[2]." ".$station[1];
+			$link = "https://www.shell.com.ru/";
+			$id=$station[0];
+		update_object_int(''.$id,$lng,$lat,$type,$name,null,$link,$index);
+		$ids[]=$id;
+	};
+	echo "Loading objects success \r\n";
+	
+	$query = $client->count(['index' => $index]);
+	$docsCount_add=1*$query['count'];
+	$docsCount_clean=$docsCount_add;
+	if (((count($ids))/$docsCount_start)*100 > 70) {
+		clean_objects($index,'must_not',$ids);
+		echo "Cleaning success \r\n";
+		$query = $client->count(['index' => $index]);
+		$docsCount_clean=1*$query['count'];
+	} else {
+		echo "Cleaning cancelled \r\n";
+	};
+	
+	if ($index_alias) { replace_index_alias($index,$index_alias); };
+	echo "Statistics. Docs added: ".($docsCount_add-$docsCount_start)." Docs cleaned: ".($docsCount_add-$docsCount_clean)." Docs now: ".$docsCount_clean."\r\n";
+} else {
 	echo "Failed to load file \r\n";
-    }
+};
 
-clean_objects($index,'must_not',$ids);
-replace_index_alias($index,"roadsituation_$index");
 echo "Finish ".date('Y-m-d H:i:s')."\r\n";
 echo "----------------------------------------\r\n";
 ?>
